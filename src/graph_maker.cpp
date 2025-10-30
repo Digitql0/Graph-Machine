aede7fa697881aefa58e2d6f5ef75e644cacc9d1
Digitql0 <imonkix@gmail.com>
2025-10-30 17:22:56 +0100

    Archived

--- src/graph_maker.cpp
+++ src/graph_maker.cpp
@@ -1,118 +1,122 @@
 #include "graph_maker.hpp"
 
+#include <iostream>
+
 bool makeGrid = true;
 bool showXY = true;
 bool showOriginLines = true;
 float maxJump = 50.0f;
 
 Graph make_graph(Vec null_value, Vec origin, float (*func)(float)) {
-  return (Graph){
-      {null_value.y}, {null_value.y}, null_value, origin, 1, 1, 1, func};
+    return (Graph){
+        {null_value.y}, {null_value.y}, null_value, origin, 1, 1, 1, func};
 }
 
 void advance_graph(Graph &g) {
-  float last_positive_element =
-      g.null_value.x + (g.positive_values.size() * g.step - g.step);
-  float next_positive_element = last_positive_element + g.step;
-  float next_positive_value = g.func(next_positive_element);
+    float last_positive_element =
+        g.null_value.x + (g.positive_values.size() * g.step - g.step);
+    float next_positive_element = last_positive_element + g.step;
+    float next_positive_value = g.func(next_positive_element);
 
-  float last_negative_element =
-      g.null_value.x - (g.positive_values.size() * g.step - g.step);
-  float next_negative_element = last_negative_element - g.step;
-  float next_negative_value = g.func(next_negative_element);
+    float last_negative_element =
+        g.null_value.x - (g.positive_values.size() * g.step - g.step);
+    float next_negative_element = last_negative_element - g.step;
+    float next_negative_value = g.func(next_negative_element);
 
-  g.positive_values.push_back(next_positive_value);
-  g.negative_values.push_back(next_negative_value);
+    g.positive_values.push_back(next_positive_value);
+    g.negative_values.push_back(next_negative_value);
 
-  for (auto &w : g.positive_values) {
-    std::cout << w << " ";
-  }
-  std::cout << std::endl;
+    for (auto &w : g.positive_values) {
+        std::cout << w << " ";
+    }
+    std::cout << std::endl;
 
-  for (auto &w : g.negative_values) {
-    std::cout << w << " ";
-  }
-  std::cout << std::endl;
+    for (auto &w : g.negative_values) {
+        std::cout << w << " ";
+    }
+    std::cout << std::endl;
 }
 
 void reduce_graph(Graph &g) {
-  if (g.positive_values.size() > 1) {
-    g.positive_values.pop_back();
-  }
+    if (g.positive_values.size() > 1) {
+        g.positive_values.pop_back();
+    }
 
-  if (g.negative_values.size() > 1) {
-    g.negative_values.pop_back();
-  }
+    if (g.negative_values.size() > 1) {
+        g.negative_values.pop_back();
+    }
 
-  for (auto &w : g.positive_values) {
-    std::cout << w << " ";
-  }
-  std::cout << std::endl;
+    for (auto &w : g.positive_values) {
+        std::cout << w << " ";
+    }
+    std::cout << std::endl;
 
-  for (auto &w : g.negative_values) {
-    std::cout << w << " ";
-  }
-  std::cout << std::endl;
+    for (auto &w : g.negative_values) {
+        std::cout << w << " ";
+    }
+    std::cout << std::endl;
 }
 
 void render_graph(Graph g, float screenWidth, float screenHeight) {
-  for (int i = 1; i < g.positive_values.size(); i++) {
-    if (std::isnan(g.positive_values[i]) || std::isinf(g.positive_values[i])) {
-      continue;
+    for (int i = 1; i < g.positive_values.size(); i++) {
+        if (std::isnan(g.positive_values[i]) ||
+            std::isinf(g.positive_values[i])) {
+            continue;
+        }
+        float current_y = -g.positive_values[i] * g.vertical_stretch;
+        float last_y = -g.positive_values[i - 1] * g.vertical_stretch;
+        if (fabs(current_y - last_y) > maxJump) {
+            continue;
+        }
+        float current_X = i * (g.horizontal_stretch * g.step);
+        float last_x = (i - 1) * (g.horizontal_stretch * g.step);
+        MGL_drawLine(g.origin.x + current_X, g.origin.y + current_y,
+                     g.origin.x + last_x, g.origin.y + last_y, 255, 255, 255);
     }
-    float current_y = -g.positive_values[i] * g.vertical_stretch;
-    float last_y = -g.positive_values[i - 1] * g.vertical_stretch;
-    if (fabs(current_y - last_y) > maxJump) {
-      continue;
-    }
-    float current_X = i * (g.horizontal_stretch * g.step);
-    float last_x = (i - 1) * (g.horizontal_stretch * g.step);
-    MGL_drawLine(g.origin.x + current_X, g.origin.y + current_y,
-                 g.origin.x + last_x, g.origin.y + last_y, 255, 255, 255);
-  }
-  for (int i = 1; i < g.negative_values.size(); i++) {
-    if (std::isnan(g.positive_values[i]) || std::isinf(g.positive_values[i])) {
-      continue;
-    }
-    float current_y = -g.negative_values[i] * g.vertical_stretch;
-    float last_y = -g.negative_values[i - 1] * g.vertical_stretch;
-    if (fabs(current_y - last_y) > maxJump) {
-      continue;
-    }
-    float current_X = i * -(g.horizontal_stretch * g.step);
-    float last_x = (i - 1) * -(g.horizontal_stretch * g.step);
-    MGL_drawLine(g.origin.x + current_X, g.origin.y + current_y,
-                 g.origin.x + last_x, g.origin.y + last_y, 255, 255, 255);
-  }
-
-  MGL_lineWidth(0.5);
-
-  if (makeGrid) {
-    for (int i = g.origin.y; i < screenHeight; i += g.vertical_stretch) {
-      MGL_drawLine(0, i, screenWidth, i, 255, 255, 255);
+    for (int i = 1; i < g.negative_values.size(); i++) {
+        if (std::isnan(g.positive_values[i]) ||
+            std::isinf(g.positive_values[i])) {
+            continue;
+        }
+        float current_y = -g.negative_values[i] * g.vertical_stretch;
+        float last_y = -g.negative_values[i - 1] * g.vertical_stretch;
+        if (fabs(current_y - last_y) > maxJump) {
+            continue;
+        }
+        float current_X = i * -(g.horizontal_stretch * g.step);
+        float last_x = (i - 1) * -(g.horizontal_stretch * g.step);
+        MGL_drawLine(g.origin.x + current_X, g.origin.y + current_y,
+                     g.origin.x + last_x, g.origin.y + last_y, 255, 255, 255);
     }
 
-    for (int i = g.origin.y; i > 0; i -= g.vertical_stretch) {
-      MGL_drawLine(0, i, screenWidth, i, 255, 255, 255);
+    MGL_lineWidth(0.5);
+
+    if (makeGrid) {
+        for (int i = g.origin.y; i < screenHeight; i += g.vertical_stretch) {
+            MGL_drawLine(0, i, screenWidth, i, 255, 255, 255);
+        }
+
+        for (int i = g.origin.y; i > 0; i -= g.vertical_stretch) {
+            MGL_drawLine(0, i, screenWidth, i, 255, 255, 255);
+        }
+
+        for (int i = g.origin.x; i < screenWidth; i += g.horizontal_stretch) {
+            MGL_drawLine(i, 0, i, screenHeight, 255, 255, 255);
+        }
+
+        for (int i = g.origin.x; i > 0; i -= g.horizontal_stretch) {
+            MGL_drawLine(i, 0, i, screenHeight, 255, 255, 255);
+        }
     }
 
-    for (int i = g.origin.x; i < screenWidth; i += g.horizontal_stretch) {
-      MGL_drawLine(i, 0, i, screenHeight, 255, 255, 255);
+    if (showOriginLines) {
+        MGL_drawLine(g.origin.x, 0, g.origin.x, screenHeight, 255, 255, 255);
+        MGL_drawLine(0, g.origin.y, screenWidth, g.origin.y, 255, 255, 255);
     }
-
-    for (int i = g.origin.x; i > 0; i -= g.horizontal_stretch) {
-      MGL_drawLine(i, 0, i, screenHeight, 255, 255, 255);
+    if (showXY) {
+        MGL_drawLine(g.origin.x, g.origin.y, g.origin.x + 200, g.origin.y, 255,
+                     0, 0);
+        MGL_drawLine(g.origin.x, g.origin.y, g.origin.x, g.origin.y + 200, 0,
+                     255, 0);
     }
-  }
-
-  if (showOriginLines) {
-    MGL_drawLine(g.origin.x, 0, g.origin.x, screenHeight, 255, 255, 255);
-    MGL_drawLine(0, g.origin.y, screenWidth, g.origin.y, 255, 255, 255);
-  }
-  if (showXY) {
-    MGL_drawLine(g.origin.x, g.origin.y, g.origin.x + 200, g.origin.y, 255, 0,
-                 0);
-    MGL_drawLine(g.origin.x, g.origin.y, g.origin.x, g.origin.y + 200, 0, 255,
-                 0);
-  }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,59 @@
+#include <iostream>
+
 #include "MiniGL.h"
 #include "graph_maker.hpp"
 
 float quadratic_function(float x) { return tan(x); }
 
 int main() {
-  int screenWidth = 1920;
-  int screenHeight = 1080;
-  float x = 0;
-  float y = 0;
+    int screenWidth = 1920;
+    int screenHeight = 1080;
+    float x = 0;
+    float y = 0;
 
-  Graph myGraph = make_graph({x, quadratic_function(x)},
-                             {(float)screenWidth / 2, (float)screenHeight / 2},
-                             quadratic_function);
-  myGraph.horizontal_stretch = 10;
-  myGraph.vertical_stretch = 50;
-  myGraph.step = 0.01;
+    Graph myGraph = make_graph(
+        {x, quadratic_function(x)},
+        {(float)screenWidth / 2, (float)screenHeight / 2}, quadratic_function);
+    myGraph.horizontal_stretch = 10;
+    myGraph.vertical_stretch = 50;
+    myGraph.step = 0.01;
 
-  MGL_makeWindow(screenWidth, screenHeight, "Graph Maker");
-  MGL_lineWidth(3);
+    MGL_makeWindow(screenWidth, screenHeight, "Graph Maker");
+    MGL_lineWidth(3);
 
-  makeGrid = false;
-  showXY = false;
-  showOriginLines = false;
+    makeGrid = false;
+    showXY = false;
+    showOriginLines = false;
 
-  while (!MGL_windowShouldClose()) {
-    MGL_beginDrawing();
-    MGL_clearBackground(0, 0, 255 / 4);
+    while (!MGL_windowShouldClose()) {
+        MGL_beginDrawing();
+        MGL_clearBackground(0, 0, 255 / 4);
 
-    render_graph(myGraph, screenWidth, screenHeight);
+        render_graph(myGraph, screenWidth, screenHeight);
 
-    if (MGL_isKeyPressed(GLFW_KEY_RIGHT)) {
-      advance_graph(myGraph);
-      std::cout << "Advanced" << std::endl;
+        if (MGL_isKeyPressed(GLFW_KEY_RIGHT)) {
+            advance_graph(myGraph);
+            std::cout << "Advanced" << std::endl;
+        }
+
+        if (MGL_isKeyPressed(GLFW_KEY_LEFT)) {
+            reduce_graph(myGraph);
+            std::cout << "Reduced" << std::endl;
+        }
+
+        if (MGL_isKeyPressed(GLFW_KEY_SPACE)) {
+            for (int i = 0; i < 1000; i++) {
+                advance_graph(myGraph);
+            }
+        }
+
+        if (MGL_isKeyPressed(GLFW_KEY_S)) {
+            y++;
+        }
+
+        MGL_drawLine(0, y, screenWidth, y, 255, 255, 255);
+        MGL_endDrawing();
     }
 
-    if (MGL_isKeyPressed(GLFW_KEY_LEFT)) {
-      reduce_graph(myGraph);
-      std::cout << "Reduced" << std::endl;
-    }
-
-    if (MGL_isKeyPressed(GLFW_KEY_SPACE)) {
-      for (int i = 0; i < 1000; i++) {
-        advance_graph(myGraph);
-      }
-    }
-
-    if (MGL_isKeyPressed(GLFW_KEY_S)) {
-      y++;
-    }
-
-    MGL_drawLine(0, y, screenWidth, y, 255, 255, 255);
-    MGL_endDrawing();
-  }
-
-  MGL_closeWindow();
+    MGL_closeWindow();
 }
